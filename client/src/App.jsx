34683548fca745f0c6a4249c4b6ea8edb17981fd
AmandaThiwa <153912255+AmandaThiwa@users.noreply.github.com>
2025-05-19 18:49:55 +0530

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
-import {BrowserRouter as Router, Route, Routes, BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import Signin from './pages/SignIn';
+import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Profile() {
   return (
-    <div><p>Profile</p></div>
+    <div>Profile</div>
   )
 }
